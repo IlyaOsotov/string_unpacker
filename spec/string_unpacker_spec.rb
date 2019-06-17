2e4eaa3c306885aa01b0f457041032dcf38839b6
IlyaOsotov <soten58@yandex.ru>
2019-06-18 01:03:05 +0300

    added tests

--- spec/string_unpacker_spec.rb
+++ spec/string_unpacker_spec.rb
@@ -3,6 +3,24 @@ RSpec.describe Unpacker do
     expect(StringUnpacker::VERSION).not_to be nil
   end
 
-  let(:string) { 'qwerty' }
-  it { expect(Unpacker.new.call(string)).to eq(string) }
+  subject { Unpacker.new }
+
+  context "when valid string" do
+    context "when only letters" do
+      let(:string) { 'asdqwe' }
+      it { expect(subject.call(string)).to eq string }
+    end
+
+    context "when complex string" do
+      it { expect(subject.call('a4bc2d5e')).to eq 'aaaabccddddde' }
+      it { expect(subject.call('qwe\4\5')).to eq 'qwe45' }
+      it { expect(subject.call('qwe\45')).to eq 'qwe44444' }
+      it { expect(subject.call('qwe\\4')).to eq 'qwe\\\\' }
+    end
+  end
+
+  context "when invalid string" do
+    let(:string) { '45' }
+    it { expect { subject.call(string) }.to raise_error } 
+  end
 end
