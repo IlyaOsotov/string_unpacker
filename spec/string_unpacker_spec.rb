aaa2b6b80e11f28c0ec5ed5b1a056663f91d73c4
IlyaOsotov <soten58@yandex.ru>
2019-06-18 20:37:11 +0300

    rewrited tests

--- spec/string_unpacker_spec.rb
+++ spec/string_unpacker_spec.rb
@@ -3,24 +3,28 @@ RSpec.describe Unpacker do
     expect(StringUnpacker::VERSION).not_to be nil
   end
 
-  subject { Unpacker.new }
+  subject { Unpacker.new.call(string) }
 
   context "when valid string" do
     context "when only letters" do
       let(:string) { 'asdqwe' }
-      it { expect(subject.call(string)).to eq string }
+      it { expect(subject).to eq string }
     end
 
     context "when complex string" do
-      it { expect(subject.call('a4bc2d5e')).to eq 'aaaabccddddde' }
-      it { expect(subject.call('qwe\4\5')).to eq 'qwe45' }
-      it { expect(subject.call('qwe\45')).to eq 'qwe44444' }
-      it { expect(subject.call('qwe\\4')).to eq 'qwe\\\\' }
+      let(:string) { 'a4bc2d5e' }
+      it { expect(subject).to eq 'aaaabccddddde' }
+      let(:string) { 'qwe\4\5' }
+      it { expect(subject).to eq 'qwe45' }
+      let(:string) { 'qwe\45' }
+      it { expect(subject).to eq 'qwe44444' }
+      let(:string) { 'qwe\\4' }
+      it { expect(subject).to eq 'qwe\\\\' }
     end
   end
 
   context "when invalid string" do
     let(:string) { '45' }
-    it { expect { subject.call(string) }.to raise_error } 
+    it { expect { subject }.to raise_error } 
   end
 end
