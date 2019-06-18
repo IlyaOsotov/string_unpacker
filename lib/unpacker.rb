f2a7a1b70e85d06c9cb8f1d0f4c1691e7baf2f57
IlyaOsotov <soten58@yandex.ru>
2019-06-18 21:29:21 +0300

    added basic interface

--- lib/unpacker.rb
+++ lib/unpacker.rb
@@ -1,7 +1,34 @@
 require "string_unpacker/version"
 
 class Unpacker
+  def initialize
+    @letters = []
+    @numbers = []
+    @result = ''
+  end
+
   def call(string)
-    string
+    string.split('').each do |letter|
+      if letter.to_i == letter
+        check_number(letter)
+      elsif letter == '\\'
+        check_slash
+      else
+        check_letter(letter)
+      end
+    end
+  end
+
+  private
+
+  attr_accessor :letters, :numbers, :result
+
+  def check_number(number)
+  end
+
+  def check_slash
+  end
+
+  def check_letter(letter)
   end
 end
